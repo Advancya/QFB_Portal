9a4fc04de0160d3785f836ec6e3a9e101bb313cc
Maggie <margret.salama@gmail.com>
2020-12-17 11:54:02 +0200

    admin screens enhancemnets

--- src/pages/Notifications/Listing.tsx
+++ src/pages/Notifications/Listing.tsx
@@ -60,160 +60,156 @@ function NotificationsListing() {
   return (
     <div>
       <Breadcrumb pageName={local_Strings.NotificationsListingTitle} />
-      <div className="main-section">
-        <div className="d-flex align-items-center my-3 justify-content-between">
-          <div className="ib-text">
-            <h3 className="mb-2">{local_Strings.NotificationsListingTitle}</h3>
+      <div className="container-fluid">
+        <div className="main-section">
+          <div className="d-flex align-items-center my-3 justify-content-between">
+            <div className="ib-text">
+              <h3 className="mb-2">
+                {local_Strings.NotificationsListingTitle}
+              </h3>
+            </div>
+            <button
+              type="button"
+              className="btn btn-sm btn-primary"
+              onClick={() =>
+                setFormAttributes({
+                  selectedItem: emptyNotificationsDetail,
+                  showForm: true,
+                  showEditable: true,
+                })
+              }
+            >
+              {local_Strings.NotificationsAddNew}
+            </button>
           </div>
-          <button
-            type="button"
-            className="btn btn-sm btn-primary mt-1"
-            style={{ marginLeft: 50 }}
-            onClick={() =>
-              setFormAttributes({
-                selectedItem: emptyNotificationsDetail,
-                showForm: true,
-                showEditable: true,
-              })
-            }
-          >
-            {local_Strings.NotificationsAddNew}
-          </button>
-        </div>
-        <button
-          type="button"
-          className="btn btn-sm btn-primary mt-1"
-          style={{ marginLeft: 50 }}
-          onClick={() =>
-            setFormAttributes({
-              selectedItem: emptyNotificationsDetail,
-              showForm: true,
-              showEditable: true,
-            })
-          }
-        >
-          {local_Strings.NotificationsAddNew}
-        </button>
-      </div>
-      <br />
-      <LoadingOverlay
-        active={isLoading}
-        spinner={
-          <PuffLoader
-            size={Constant.SpnnerSize}
-            color={Constant.SpinnerColor}
-          />
-        }
-      />
-      <div className="card-header-search">
-        <div className="row align-items-center">
-          <div className="col-md-8">
-            <div className="field-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder={local_Strings.searchPlaceholder}
-                onChange={(e) => {
-                  if (!!e.target.value) {
-                    const _filteredData = [...data];
-                    setFilteredData(
-                      _filteredData
-                        .filter(
-                          (f) =>
-                            Object.values(f).filter(
-                              (t: any) =>
-                                t &&
-                                t
-                                  .toString()
-                                  .toLowerCase()
-                                  .indexOf(e.target.value.toLowerCase()) !== -1
-                            ).length > 0
-                        )
-                        .slice(0, 10)
-                    );
-                  } else {
-                    setFilteredData(data.slice(0, 10));
-                  }
-                }}
+          <LoadingOverlay
+            active={isLoading}
+            spinner={
+              <PuffLoader
+                size={Constant.SpnnerSize}
+                color={Constant.SpinnerColor}
               />
-              <div className="demandDateValue searchInputIcon">
-                <i className="fa fa-search"></i>
+            }
+          />
+          <div className="card-header-search">
+            <div className="row align-items-center">
+              <div className="col-md-12 mb-4">
+                <div className="field-group">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder={local_Strings.searchPlaceholder}
+                      onChange={(e) => {
+                        if (!!e.target.value) {
+                          const _filteredData = [...data];
+                          setFilteredData(
+                            _filteredData
+                              .filter(
+                                (f) =>
+                                  Object.values(f).filter(
+                                    (t: any) =>
+                                      t &&
+                                      t
+                                        .toString()
+                                        .toLowerCase()
+                                        .indexOf(
+                                          e.target.value.toLowerCase()
+                                        ) !== -1
+                                  ).length > 0
+                              )
+                              .slice(0, 10)
+                          );
+                        } else {
+                          setFilteredData(data.slice(0, 10));
+                        }
+                      }}
+                    />
+                    <div className="input-group-append">
+                      <span className="input-group-text" id="inputGroupPrepend">
+                        <div className="demandDateValue searchInputIcon">
+                          <i className="fa fa-search"></i>
+                        </div>
+                      </span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
 
-        <div className="box modal-box py-0 mb-0 scrollabel-modal-box">
-          <ul className="box-list" id="dataList">
-            {filteredData &&
-              filteredData.length > 0 &&
-              filteredData.map((item, index) => (
-                <li className="shown" key={index}>
-                  <a
-                    href="#"
-                    className="row align-items-center"
-                    onClick={() =>
-                      setFormAttributes({
-                        selectedItem: item,
-                        showForm: true,
-                        showEditable: false,
-                      })
-                    }
-                  >
-                    <div className="col-12 col-sm-12">
-                      <div className="mb-1 d-flex align-items-center">
-                        <img src={dateIcon} className="img-fluid" />
-                        <span className="mx-1 text-15 color-light-gold">
-                          {item.messageSendDate
-                            ? moment(item.messageSendDate).format(
-                                "dddd DD MM YYYY"
-                              )
-                            : ""}
-                        </span>
-                      </div>
-                      <h6 className="mb-1 text-600">
-                        {item.messageTitle || item.messageSubTitle || ""}
-                      </h6>
-                      <div className="text-15">
-                        {local_Strings.NotificationsExpireLabel +
-                          " " +
-                          (item.expiryDate
-                            ? moment(item.expiryDate).format("DD-MM-YYYY")
-                            : "")}
-                      </div>
-                    </div>
-                  </a>
-                </li>
-              ))}
-          </ul>
+            <div className="box modal-box py-0 mb-4 scrollabel-modal-box">
+              <ul className="box-list" id="dataList">
+                {filteredData &&
+                  filteredData.length > 0 &&
+                  filteredData.map((item, index) => (
+                    <li className="shown" key={index}>
+                      <a
+                        href="#"
+                        className="row align-items-center"
+                        onClick={() =>
+                          setFormAttributes({
+                            selectedItem: item,
+                            showForm: true,
+                            showEditable: false,
+                          })
+                        }
+                      >
+                        <div className="col-12 col-sm-12">
+                          <div className="mb-1 d-flex align-items-center">
+                            <img src={dateIcon} className="img-fluid" />
+                            <span className="mx-1 text-15 color-light-gold">
+                              {item.messageSendDate
+                                ? moment(item.messageSendDate).format(
+                                    "dddd DD MM YYYY"
+                                  )
+                                : ""}
+                            </span>
+                          </div>
+                          <h6 className="mb-1 text-600">
+                            {item.messageTitle || item.messageSubTitle || ""}
+                          </h6>
+                          <div className="text-15">
+                            {local_Strings.NotificationsExpireLabel +
+                              " " +
+                              (item.expiryDate
+                                ? moment(item.expiryDate).format("DD-MM-YYYY")
+                                : "")}
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            <NotificationsForm
+              item={formAttributes.selectedItem}
+              show={formAttributes.showForm}
+              editable={formAttributes.showEditable}
+              OnHide={() =>
+                setFormAttributes({
+                  ...formAttributes,
+                  showForm: false,
+                })
+              }
+              OnBack={() =>
+                setFormAttributes({
+                  ...formAttributes,
+                  showForm: false,
+                })
+              }
+              refreshList={() => refreshList()}
+            />
+          </div>
+          {data.length > 10 && (
+            <Pagination
+              items={data as []}
+              onChangePage={setFilteredData}
+              initialPage={1}
+              pageSize={10}
+            />
+          )}
         </div>
-        <NotificationsForm
-          item={formAttributes.selectedItem}
-          show={formAttributes.showForm}
-          editable={formAttributes.showEditable}
-          OnHide={() =>
-            setFormAttributes({
-              ...formAttributes,
-              showForm: false,
-            })
-          }
-          OnBack={() =>
-            setFormAttributes({
-              ...formAttributes,
-              showForm: false,
-            })
-          }
-          refreshList={() => refreshList()}
-        />
       </div>
-      {data.length > 10 && (
-        <Pagination
-          items={data as []}
-          onChangePage={setFilteredData}
-          initialPage={1}
-          pageSize={10}
-        />
-      )}
       <NotificationsForm
         item={formAttributes.selectedItem}
         show={formAttributes.showForm}
--- src/pages/Offers/Listing.tsx
+++ src/pages/Offers/Listing.tsx
@@ -76,191 +76,189 @@ function OffersListing() {
   return (
     <div>
       <Breadcrumb pageName={local_Strings.OffersListingTitle} />
-      <div className="main-section">
-        <div className="d-flex align-items-center my-3 justify-content-between">
-          <div className="ib-text">
-            <h3 className="">{local_Strings.OffersListingTitle}</h3>
+      <div className="container-fluid">
+        <div className="main-section">
+          <div className="d-flex align-items-center my-3 justify-content-between">
+            <div className="ib-text">
+              <h3 className="">{local_Strings.OffersListingTitle}</h3>
+            </div>
+            <button
+              type="button"
+              className="btn btn-sm btn-primary"
+              onClick={() =>
+                setFormAttributes({
+                  selectedItem: emptyOfferData,
+                  showForm: true,
+                  showEditable: true,
+                })
+              }
+            >
+              {local_Strings.OfferAddNew}
+            </button>
           </div>
-          <button
-            type="button"
-            className="btn btn-sm btn-primary"
-            onClick={() =>
-              setFormAttributes({
-                selectedItem: emptyOfferData,
-                showForm: true,
-                showEditable: true,
-              })
-            }
-          >
-            {local_Strings.OfferAddNew}
-          </button>
-        </div>
-        <button
-          type="button"
-          className="btn btn-sm btn-primary mt-1"
-          style={{ marginLeft: 50 }}
-          onClick={() =>
-            setFormAttributes({
-              selectedItem: emptyOfferData,
-              showForm: true,
-              showEditable: true,
-            })
-          }
-        >
-          {local_Strings.OfferAddNew}
-        </button>
-      </div>
-      <br />
-      <LoadingOverlay
-        active={isLoading}
-        spinner={
-          <PuffLoader
-            size={Constant.SpnnerSize}
-            color={Constant.SpinnerColor}
-          />
-        }
-      />
-      <div className="card-header-search">
-        <div className="row align-items-center">
-          <div className="col-md-8">
-            <div className="field-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder={local_Strings.searchPlaceholder}
-                onChange={(e) => {
-                  if (!!e.target.value) {
-                    const _filteredData = [...data];
-                    setFilteredData(
-                      _filteredData
-                        .filter(
-                          (f) =>
-                            Object.values(f).filter(
-                              (t: any) =>
-                                t &&
-                                t
-                                  .toString()
-                                  .toLowerCase()
-                                  .indexOf(e.target.value.toLowerCase()) !== -1
-                            ).length > 0
-                        )
-                        .slice(0, 10)
-                    );
-                  } else {
-                    setFilteredData(data.slice(0, 10));
-                  }
-                }}
-              />
-              <div className="demandDateValue searchInputIcon">
-                <i className="fa fa-search"></i>
+
+          <div className="card-header-search">
+            <div className="row align-items-center">
+              <div className="col-md-12  mb-4">
+                <div className="field-group">
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder={local_Strings.searchPlaceholder}
+                      onChange={(e) => {
+                        if (!!e.target.value) {
+                          const _filteredData = [...data];
+                          setFilteredData(
+                            _filteredData
+                              .filter(
+                                (f) =>
+                                  Object.values(f).filter(
+                                    (t: any) =>
+                                      t &&
+                                      t
+                                        .toString()
+                                        .toLowerCase()
+                                        .indexOf(
+                                          e.target.value.toLowerCase()
+                                        ) !== -1
+                                  ).length > 0
+                              )
+                              .slice(0, 10)
+                          );
+                        } else {
+                          setFilteredData(data.slice(0, 10));
+                        }
+                      }}
+                    />
+                    <div className="input-group-append">
+                      <span className="input-group-text" id="inputGroupPrepend">
+                        <div className="demandDateValue searchInputIcon">
+                          <i className="fa fa-search"></i>
+                        </div>
+                      </span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
+            <LoadingOverlay
+              active={isLoading}
+              spinner={
+                <PuffLoader
+                  size={Constant.SpnnerSize}
+                  color={Constant.SpinnerColor}
+                />
+              }
+            />
+            <div className="box modal-box py-0 mb-4 scrollabel-modal-box">
+              <ul className="box-list" id="dataList">
+                {filteredData &&
+                  filteredData.length > 0 &&
+                  filteredData.map((item, index) => (
+                    <li className="shown" key={index}>
+                      <a
+                        onClick={() => {
+                          confirmAlert({
+                            title: local_Strings.deleteSure,
+                            message: local_Strings.deleteSureMessage,
+                            buttons: [
+                              {
+                                label: local_Strings.OfferDeleteButton,
+                                onClick: () => deleteTheRecord(item.id),
+                              },
+                              {
+                                label: local_Strings.cancelBtn,
+                                onClick: () => {},
+                              },
+                            ],
+                          });
+                        }}
+                        style={{ cursor: "pointer", float: "right" }}
+                      >
+                        {local_Strings.OfferDeleteButton}
+                      </a>
+                      <a
+                        onClick={() =>
+                          setFormAttributes({
+                            selectedItem: item,
+                            showForm: true,
+                            showEditable: true,
+                          })
+                        }
+                        style={{ cursor: "pointer", float: "right" }}
+                      >
+                        {local_Strings.BeneficiaryEditButton}
+                      </a>
+
+                      <a
+                        href="#"
+                        className="row align-items-center"
+                        onClick={() =>
+                          setFormAttributes({
+                            selectedItem: item,
+                            showForm: true,
+                            showEditable: false,
+                          })
+                        }
+                      >
+                        <div className="col-12 col-sm-12">
+                          <div className="mb-1 d-flex align-items-center">
+                            <img src={dateIcon} className="img-fluid" />
+                            <span className="mx-1 text-15 color-light-gold">
+                              {item.createdDate
+                                ? moment(item.createdDate).format(
+                                    "dddd DD MM YYYY"
+                                  )
+                                : ""}
+                            </span>
+                          </div>
+                          <h6 className="mb-1 text-600">
+                            {auth.language === "en" ? item.title : item.titleAr}
+                          </h6>
+                          <div className="text-15">
+                            {local_Strings.NotificationsExpireLabel +
+                              " " +
+                              (item.expireDate
+                                ? moment(item.expireDate).format("DD-MM-YYYY")
+                                : "")}
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            <OffersForm
+              itemID={formAttributes.selectedItem.id}
+              show={formAttributes.showForm}
+              editable={formAttributes.showEditable}
+              OnHide={() =>
+                setFormAttributes({
+                  ...formAttributes,
+                  showForm: false,
+                })
+              }
+              OnBack={() =>
+                setFormAttributes({
+                  ...formAttributes,
+                  showForm: false,
+                })
+              }
+              refreshList={() => refreshList()}
+            />
           </div>
+          {data.length > 10 && (
+            <Pagination
+              items={data as []}
+              onChangePage={setFilteredData}
+              initialPage={1}
+              pageSize={10}
+            />
+          )}
         </div>
-
-        <div className="box modal-box py-0 mb-0 scrollabel-modal-box">
-          <ul className="box-list" id="dataList">
-            {filteredData &&
-              filteredData.length > 0 &&
-              filteredData.map((item, index) => (
-                <li className="shown" key={index}>
-                  <a
-                    onClick={() => {
-                      confirmAlert({
-                        title: local_Strings.deleteSure,
-                        message: local_Strings.deleteSureMessage,
-                        buttons: [
-                          {
-                            label: local_Strings.OfferDeleteButton,
-                            onClick: () => deleteTheRecord(item.id),
-                          },
-                          {
-                            label: local_Strings.cancelBtn,
-                            onClick: () => {},
-                          },
-                        ],
-                      });
-                    }}
-                    style={{ cursor: "pointer", float: "right" }}
-                  >
-                    {local_Strings.OfferDeleteButton}
-                  </a>
-                  <a
-                    onClick={() =>
-                      setFormAttributes({
-                        selectedItem: item,
-                        showForm: true,
-                        showEditable: true,
-                      })
-                    }
-                    style={{ cursor: "pointer", float: "right" }}
-                  >
-                    {local_Strings.BeneficiaryEditButton}
-                  </a>
-
-                  <a
-                    href="#"
-                    className="row align-items-center"
-                    onClick={() =>
-                      setFormAttributes({
-                        selectedItem: item,
-                        showForm: true,
-                        showEditable: false,
-                      })
-                    }
-                  >
-                    <div className="col-12 col-sm-12">
-                      <div className="mb-1 d-flex align-items-center">
-                        <img src={dateIcon} className="img-fluid" />
-                        <span className="mx-1 text-15 color-light-gold">
-                          {item.createdDate
-                            ? moment(item.createdDate).format("dddd DD MM YYYY")
-                            : ""}
-                        </span>
-                      </div>
-                      <h6 className="mb-1 text-600">
-                        {auth.language === "en" ? item.title : item.titleAr}
-                      </h6>
-                      <div className="text-15">
-                        {local_Strings.NotificationsExpireLabel +
-                          " " +
-                          (item.expireDate
-                            ? moment(item.expireDate).format("DD-MM-YYYY")
-                            : "")}
-                      </div>
-                    </div>
-                  </a>
-                </li>
-              ))}
-          </ul>
-        </div>
-        <OffersForm
-          itemID={formAttributes.selectedItem.id}
-          show={formAttributes.showForm}
-          editable={formAttributes.showEditable}
-          OnHide={() =>
-            setFormAttributes({
-              ...formAttributes,
-              showForm: false,
-            })
-          }
-          OnBack={() =>
-            setFormAttributes({
-              ...formAttributes,
-              showForm: false,
-            })
-          }
-          refreshList={() => refreshList()}
-        />
       </div>
-      {data.length > 10 && (
-        <Pagination
-          items={data as []}
-          onChangePage={setFilteredData}
-          initialPage={1}
-          pageSize={10}
-        />
-      )}
+
       <OffersForm
         itemID={formAttributes.selectedItem.id}
         show={formAttributes.showForm}
