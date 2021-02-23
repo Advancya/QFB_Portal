18a3365b13680db4305ac06dff57d83fec338aea
Feras <eng.feras21@outlook.com>
2021-02-23 16:08:57 +0200

    Latest

--- src/constants/defaultData.ts
+++ src/constants/defaultData.ts
@@ -1,7 +1,10 @@
-export default { 
-  IdentityBaseUrl: "https://identitydev.qfbonline.com",
-  ApiBaseUrl: "https://apidev.qfbonline.com",
-  PortalUrl: "https://ibdev.qfbonline.com",
+export default {
+  // IdentityBaseUrl: "https://identitydev.qfbonline.com",
+  // ApiBaseUrl: "https://apidev.qfbonline.com",
+  // PortalUrl: "https://ibdev.qfbonline.com",
+  IdentityBaseUrl: "https://identity.qfb.com.qa",
+  ApiBaseUrl: "https://api.qfb.com.qa",
+  PortalUrl: "https://ib.qfb.com.qa",
   AnonymousTermsAcceptanceStorageKey: "@storage_AcceptTermsStatusForAnonymous1",
   CustomerTermsAcceptanceStorageKey: "@storage_AcceptTermsStatusForCustomer",
   LoginDetailsStorageKey: "storage_LoginUsernameAndPassword",
